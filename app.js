733ced2f917d433229174af1a886403f06edaba5
Elizabeth Hale <lizzhale@mac.com>
2015-03-22 16:14:31 -0700

    Added routing to allow adding and editing a tweet

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var pg = require('pg);
+var pg = require('pg');
 var models = require('./models/index.js');
 
 var app = express();
-var server = app.list(3000);
+var server = app.listen(3000);
 
 app.use(express.static('assets'));
 
@@ -17,4 +17,38 @@ app.use(bodyParser.urlencoded({
 app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  models.Chirp.findAll({order: 'id DESC'}).done(function(error, chirps) {
+    res.render('index', {
+      chirp: chirps 
+    });
+  });
+});
+
+app.post('/add', function (req, res) {
+  models.Chirp.create({
+    chirp: req.body.chirp
+  }).done(function(error, data) {
+    res.redirect('/');
+  });
+});
+
+app.get('/edit/:id', function (req, res) {
+  models.Chirp.find(req.params.id).done(function(error, chirps) {
+    res.render('edit', {
+      chirp: chirps
+    });
+  });
+});
+
+app.put('/edit/:id', function (req, res) {
+  models.Chirp.find(req.params.id).done(function(error, chirps) {
+    chirps.updateAttributes({
+      chirp: req.body.chirp
+    }).done(function(error, data) {
+      res.redirect('/');
+    });
+  });
+});
 
